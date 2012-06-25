65a22d3fd718baecf2dcc8758369fb35a3a08496
opsb <oliver@opsb.co.uk>
2012-06-25 11:01:48 +0200

    Extract english pronouns

--- lib/verb.rb
+++ lib/verb.rb
@@ -18,15 +18,32 @@ module Verbs
     private
     def build_conjugations(verb)
       verb.map do |tense_name, conjugations|
-        conjugations.map do |english_with_discriminator, spanish|
-          build_conjugation(tense_name, english_with_discriminator, spanish)
+        i = 0
+        conjugations.map do |(hash)|
+          english_with_discriminator, spanish = hash.keys.first, hash.values.first     
+          conjugation = build_conjugation(tense_name, english_with_discriminator, spanish, pronoun(i, tense_name))
+          i += 1
+          conjugation
         end.flatten
       end.flatten
     end
     
-    def build_conjugation(tense_name, english, spanish)
+    def pronoun(index, tense)
+      pronouns = [
+        "I",
+        "You",
+        "He/She/They",
+        "We",
+        "You (all)",
+        "They (all)"
+      ]
+      return "" if tense == :gerundio
+      pronouns[index]      
+    end
+    
+    def build_conjugation(tense_name, english, spanish, pronoun)
       details = [@verb_discriminator, tense_name].compact.join("/")
-      Conjugation.new(tense_name.to_s, spanish, english + "(" + [@verb_discriminator, tense_name].compact.join(",") + ")") 
+      Conjugation.new(tense_name.to_s, spanish, pronoun + " " + english + "(" + [@verb_discriminator, tense_name].compact.join(",") + ")") 
     end    
   end
 end
