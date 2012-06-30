f9c4df7b2176fb6e11344c992be8a7105bde38d0
opsb <oliver@opsb.co.uk>
2012-06-30 13:32:13 +0200

    Only include name of tense if there is more than one

--- lib/verb.rb
+++ lib/verb.rb
@@ -1,14 +1,14 @@
 module Verbs
   class Verb
     def initialize(path)
-      verb = YAML::load(File.read(path))
-      @infinitive = verb.delete(:infinitive)
-      @verb_discriminator = verb.delete(:discriminator)    
-      @examples = build_conjugations(verb)
+      @verb = YAML::load(File.read(path))
+      @infinitive = @verb.delete(:infinitive)
+      @verb_discriminator = @verb.delete(:discriminator)    
     end
     
     def examples(tenses)
-      @examples.reject{ |e| tenses && !tenses.include?(e.tense) }.map(&:text)
+      opts = { :include_tense => true } if tenses.size > 1
+      build_conjugations(@verb).reject{ |e| tenses && !tenses.include?(e.tense) }.map(&:text)
     end    
     
     def infinitive
@@ -16,12 +16,12 @@ module Verbs
     end
     
     private
-    def build_conjugations(verb)
+    def build_conjugations(verb, opts={})
       verb.map do |tense_name, conjugations|
         i = 0
         conjugations.map do |(hash)|
           english_with_discriminator, spanish = hash.keys.first, hash.values.first     
-          conjugation = build_conjugation(tense_name, english_with_discriminator, spanish, pronoun(i, tense_name))
+          conjugation = build_conjugation(tense_name, english_with_discriminator, spanish, pronoun(i, tense_name), opts)
           i += 1
           conjugation
         end.flatten
@@ -41,9 +41,13 @@ module Verbs
       pronouns[index]      
     end
     
-    def build_conjugation(tense_name, english, spanish, pronoun)
-      details = [@verb_discriminator, tense_name].compact.join("/")
-      Conjugation.new(tense_name.to_s, spanish, pronoun + " " + english + " - " + [@verb_discriminator, tense_name].compact.join(", ")) 
+    def build_conjugation(tense_name, english, spanish, pronoun, opts={})
+      details = [@verb_discriminator]
+      details << tense_name if opts[:include_tense]
+      details = details.compact.join(", ")
+      english_with_details = english + (details.empty? ? "" : " - #{details}")
+      
+      Conjugation.new(tense_name.to_s, spanish, pronoun + " " + english_with_details) 
     end    
   end
 end
